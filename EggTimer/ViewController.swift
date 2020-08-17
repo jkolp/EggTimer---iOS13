dcb45c0828b556cf11a53c7d47733da11b025c10
jkolp <jkolp.dev@gmail.com>
2020-08-17 14:10:43 -0700

    adding project

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    let eggTimes:[String:Int] = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        progressBar.isHidden = true
+    }
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        progressBar.isHidden = false
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Egg Timer
+
+A beautiful egg timer app to boil your eggs to perfection depending on how you prefer your eggs. 
+
+* Swift Collection types - Dictionaries
+* The Swift Timer API
+* Conditional statements - IF/ELSE
+* Conditional statements - Switch
+* Functions with outputs
+* ProgressView
+
+
+![End Banner](Documentation/readme-end-banner.png)
+
